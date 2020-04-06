e349a74bf3f70cfe532852a02b45852eee3cf923
uxsasukexnaruto <nguyha16@gmail.com>
2020-04-06 11:25:49 -0700

    exploring React

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+// const App = () => (
+//     <div>
+//         <h2>My Todo List</h2>
+//         <GroceryList />
+//     </div>
+// );
+const Bread = (props) => (
+    <li>{props.item}</li>
+)
+
+const Milk = (props) => (
+    <li>{props.item}</li>
+)
+
+const GroceryList = () => (
+    <ul>
+        <Bread item="bread" />
+        <Milk item="milk" />
+    </ul>
+)
+
+
+
+
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
